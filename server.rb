a15232b1d411d1dd5439c497209ef422efba57fa
Stevie <steviepinero@gmail.com>
2016-03-29 05:06:05 -0400

    trying to load js in heroku

--- server.rb
+++ server.rb
@@ -1,6 +1,19 @@
 require 'rubygems'
 require 'sinatra'
+require 'sinatra-assetpack'
 
 get '/' do
   redirect '/index.html'
+end
+
+
+Sinatra::Application.register Sinatra::AssetPack
+
+assets do
+  serve '/js', :from => "assets/js"
+  serve '/css', :from => "assets/css"
+
+  css :main, ["/css/foundation.css","/css/offcanvas.css","/css/app.css"]
+  js :main, ["/js/jquery.min.js","/js/holder.js","/js/holder.min.js","/js/jquery.maginific-popup.min.js","/js/masonry.js","js/odometer.js","/js/pushy.min.js","/js/scripts.js","/js/wow.min.js"]
+
 end
